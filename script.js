e8e4d9989f4ab50bbead2e672ded642bc7e49bd0
hulchenko <74687999+hulchenko@users.noreply.github.com>
2021-02-19 12:54:00 -0700

    tweak html, css, finalize js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const addItems = document.querySelector('.add-items');
-const itemsList = document.querySelector('.plates');
-const items = [];//storing data in here
+/*const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.lines');
+const items = JSON.parse(localStorage.getItem('items')) || [];// #1 storing data in here
+                                                              // #2 JSON parse reverses string conversion, back to objects
 
 function addItem(e) {
     e.preventDefault();
@@ -10,7 +11,76 @@ function addItem(e) {
         done: false
     }
     items.push(item);
+    populateList(items, itemsList);
+    localStorage.setItem('items', JSON.stringify(items));//convert to strings, local Storage only saves strings
     this.reset();
 }
 
+
+
+function populateList(lines = [], linesList) {
+    linesList.innerHTML = lines.map((line, i) => {
+        return `
+        <li>
+            <input type="checkbox" data-index=${i} id="item${i}" ${line.done ? "checked" : ""}/>
+            <label for="item${i}">${line.text}</label>
+        </li>
+        `;
+    }).join("");
+}
+
 addItems.addEventListener("submit", addItem);
+populateList(items, itemsList); //pulls stored info from the const items in the beginning of the page, by the end of the page processing.
+*/
+
+const addItems = document.querySelector('.add-items');
+const itemsList = document.querySelector('.lines');
+let items = JSON.parse(localStorage.getItem('items')) || [];
+
+function addItem(e) {
+  e.preventDefault();
+  const text = (this.querySelector('[name=item]')).value;
+  const item = {
+    text,
+    done: false
+  };
+
+  items.push(item);
+  populateList(items, itemsList);
+  localStorage.setItem('items', JSON.stringify(items));
+  this.reset();
+}
+
+function populateList(lines = [], linesList) {
+  linesList.innerHTML = lines.map((line, i) => {
+    return `
+      <li>
+        <input type="checkbox" data-index=${i} id="item${i}" ${line.done ? 'checked' : ''} />
+        <label for="item${i}">${line.text}</label>
+      </li>
+    `;
+  }).join('');
+}
+
+function toggleDone(e) {
+  if (!e.target.matches('input')) return; // skip this unless it's an input
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItem('items', JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
+
+addItems.addEventListener('submit', addItem);
+itemsList.addEventListener('click', toggleDone);
+
+populateList(items, itemsList);
+
+    ////reset button
+document.querySelector(".reset").addEventListener("click", resetLocalData); 
+
+function resetLocalData() {
+    localStorage.clear();
+    window.location.reload();
+}
